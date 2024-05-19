94d05adc5084569b1fcb93250585b1450ba3aefb
Vitor Melo <89869031+VitorMeloDev@users.noreply.github.com>
2024-05-18 23:00:41 -0300

    Food Flight

--- Prototype 2/Assets/Scripts/PlayerController.cs
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10f;
     public float xRange = 10f;
+    public GameObject projectilePrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -26,6 +27,11 @@ public class PlayerController : MonoBehaviour
         else if(transform.position.x > xRange)
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
 
         horizontalInput = Input.GetAxis("Horizontal");
